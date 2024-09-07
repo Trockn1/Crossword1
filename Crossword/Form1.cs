6fc12714035454a4eeb74fafda623b9af1810789
Andrii <143174044+Trocknreal@users.noreply.github.com>
2024-09-07 11:12:12 +0300

    Добавьте файлы проекта.

---
+++ Crossword/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Crossword
+{
+    public partial class Form1 : Form
+    {
+        Clues clue_window = new Clues();
+        List<id_cells> idc = new List<id_cells>();
+        public string puzzle_file = Application.StartupPath + "\\Puzzles\\puzzle.pzl";
+
+        public Form1()
+        {
+            buildWordList();
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buildWordList()
+        {
+            String line = "";
+            using (StreamReader s = new StreamReader(puzzle_file))
+            {
+                line = s.ReadLine(); 
+                while ((line = s.ReadLine()) != null)
+                {
+                    String[] l = line.Split('|');
+                    idc.Add(new id_cells(Int32.Parse(l[0]), Int32.Parse(l[1]), l[2], l[3], l[4], l[5]));
+                    clue_window.clue_table.Rows.Add(new String[] { l[3], l[2], l[5] });
+                }
+            }
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializedBoard();
+            clue_window.SetDesktopLocation(this.Location.X + this.Width + 1, this.Location.Y);
+            clue_window.StartPosition = FormStartPosition.Manual;
+            clue_window.Show();
+            clue_window.clue_table.AutoResizeColumns();
+        }
+
+        private void InitializedBoard()
+        {
+            board.BackgroundColor = Color.Black;
+            board.DefaultCellStyle.BackColor = Color.Black;
+            for (int i = 0; i < 21; i++)
+                board.Rows.Add();
+
+            //  Встановити ширину стовпців
+            foreach (DataGridViewColumn c in board.Columns)
+                c.Width = board.Width / board.Columns.Count;
+
+            // Встановити ширину рядків
+            foreach (DataGridViewRow r in board.Rows)
+                r.Height = board.Height / board.Rows.Count;
+
+           
+            for (int row = 0; row < board.Rows.Count; row++)
+            {
+                for (int col = 0; col < board.Columns.Count; col++)
+                    board[col, row].ReadOnly = true;
+            }
+
+            foreach (id_cells i in idc)
+            {
+                int start_col = i.X;
+                int start_row = i.Y;
+                char[] word = i.word.ToCharArray();
+                for (int j = 0; j < word.Length; j++)
+                {
+                    if (i.direction.ToUpper() == "ACROSS")
+                        formatCell(start_row, start_col + j, word[j].ToString());
+
+                    if (i.direction.ToUpper() == "DOWN")
+                        formatCell(start_row + j, start_col, word[j].ToString());
+                }
+            }
+        }
+
+        private void formatCell(int row, int col, String letter)
+        {
+            DataGridViewCell c = board[col, row];
+            c.Style.BackColor = Color.White;
+            c.ReadOnly = false;
+            c.Style.SelectionBackColor = Color.Cyan;
+            c.Tag = letter;
+        }
+
+        private void Form1_LocationChanged(object sender, EventArgs e)
+        {
+            clue_window.SetDesktopLocation(this.Location.X + this.Width + 1, this.Location.Y);
+        }
+
+        private void board_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            // великі літери
+            try
+            {
+                board[e.ColumnIndex, e.RowIndex].Value = board[e.ColumnIndex, e.RowIndex].Value.ToString().ToUpper();
+            }
+            catch
+            {
+
+            }
+            // скоротити до однією букви
+            try
+            {
+                if (board[e.ColumnIndex, e.RowIndex].Value.ToString().Length > 1)
+                    board[e.ColumnIndex, e.RowIndex].Value = board[e.ColumnIndex, e.RowIndex].Value.ToString().Substring(0, 1);
+            }
+            catch
+            {
+
+            }
+
+            // Колір 
+            try
+            {
+                if (board[e.ColumnIndex, e.RowIndex].Value.ToString().ToUpper().Equals(board[e.ColumnIndex, e.RowIndex].Tag.ToString().ToUpper()))
+                    board[e.ColumnIndex, e.RowIndex].Style.ForeColor = Color.Green;
+                else
+                    board[e.ColumnIndex, e.RowIndex].Style.ForeColor = Color.Red;
+
+            }
+            catch
+            {
+
+            }
+
+            
+            CheckForWin();
+        }
+
+        private void CheckForWin()
+        {
+            bool allCorrect = true;
+            foreach (id_cells i in idc)
+            {
+                int start_col = i.X;
+                int start_row = i.Y;
+                char[] word = i.word.ToCharArray();
+                for (int j = 0; j < word.Length; j++)
+                {
+                    int col = i.direction.ToUpper() == "ACROSS" ? start_col + j : start_col;
+                    int row = i.direction.ToUpper() == "DOWN" ? start_row + j : start_row;
+
+                    if (col >= board.ColumnCount || row >= board.RowCount)
+                    {
+                        allCorrect = false;
+                        break;
+                    }
+
+                    if (board[col, row].Value == null ||
+                        !board[col, row].Value.ToString().ToUpper().Equals(word[j].ToString().ToUpper()))
+                    {
+                        allCorrect = false;
+                        break;
+                    }
+                }
+
+                if (!allCorrect)
+                    break;
+            }
+
+            if (allCorrect)
+            {
+                MessageBox.Show("Congratulations! ", "Victory", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void board_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            string number = "";
+            if (idc.Any(c => (number = c.number) != "" && c.X == e.ColumnIndex && c.Y == e.RowIndex))
+            {
+                Rectangle r = new Rectangle(e.CellBounds.X, e.CellBounds.Y, e.CellBounds.Width, e.CellBounds.Height);
+                e.Graphics.FillRectangle(Brushes.White, r);
+                Font f = new Font(e.CellStyle.Font.FontFamily, 7);
+                e.Graphics.DrawString(number, f, Brushes.Black, r);
+                e.PaintContent(e.ClipBounds);
+                e.Handled = true;
+            }
+        }
+
+        private void board_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+
+    public class id_cells
+    {
+        public int X;
+        public int Y;
+        public String direction;
+        public String number;
+        public String word;
+        public String clue;
+
+        public id_cells(int x, int y, string d, string n, string w, string c)
+        {
+            this.X = x;
+            this.Y = y;
+            this.direction = d;
+            this.number = n;
+            this.word = w;
+            this.clue = c;
+        }
+    }
+}
